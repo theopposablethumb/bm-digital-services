f32ac0904c41ebf2d88e612d0673139e2b699a20
Brendan Meachen <BMeachen1@corpuk.net>
2019-09-12 09:37:12 +0100

    Responsive styling fixes
    Fixed some styling issues for various breakpoints. Profile image on the home page now behaves and keeps its aspect ratio across various screen sizes, which required a complete restructuring of the HTML.
    Cards on individual photo pages are now more responsive so more of the photo can be seen on screen. Meta data was resized for landscape orientation to avoid it breaking and ensure it's still legible

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,7 +4,9 @@ class Hero extends React.Component {
     render () {
         return(
             <div id="hero">
-		        <img src="bren-sm.jpg" alt="Portrait of Brendan Meachen" />
+				<div className="profile">
+		        	<div></div>
+				</div>
 				<div>
 			        <h1><span>Hello,</span><br /><span>I'm Brendan</span></h1>
 			        <p>I'm a consultant with 15 years experience working with Websites and Digital Experiences</p>
